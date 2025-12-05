b47e5e2dda3d587a3bab7984edce1e650881c863
running-terminal-commands <runningterminalcommands@gmail.com>
2025-12-05 20:09:28 +0900

    保存用

---
+++ readme/copy_dev_to_main_readme.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# ./copy_dev_to_main_readme.sh
+# "$(find . -type f -name "copy_dev_to_main_readme.sh")"
+
+
+shopt -s expand_aliases
+source ~/.bash_profile
+
+START_TIME="$(date +%s.%N)"
+file_absolute_path="$(readlink -f "$0")"
+project_root_path="$(dirname "$(dirname "$file_absolute_path")")"
+echo "$file_absolute_path"
+
+git checkout dev
+dev_README="$(cat README.md)"
+sleep 5
+git checkout main
+echo "$dev_README" >> README.md
+push_main
+sleep 5
+git checkout main
+
+END_TIME="$(date +%s.%N)"
+ELAPSED_TIME="$(echo "$END_TIME - $START_TIME" | bc)"
+echo "実行時間:""$ELAPSED_TIME""s"
